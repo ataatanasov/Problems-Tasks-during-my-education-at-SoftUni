4013eddb4354ac6c5f9dacc11d2da7b9d4a3b2d3
Atanas Atanasov <ataatanasov@outlook.com>
2022-05-22 22:32:20 +0300

    Added solutions to the homework tasks (Data Types & Variables)

---
+++ Fundamentals Module/Data Types & Variables/2.charsToString.js
@@ -0,0 +1,8 @@
+function charsToString(char1, char2, char3) {
+    console.log(`${char1}${char2}${char3}`) // 1-vi nachin
+    let converted = char1 + char2 + char3; // 2- ri nachin
+    console.log(converted); // 3-ti nachin
+}
+charsToString('a', 'b', 'c')
+charsToString('%','2','o')
+charsToString('1','5','p')
