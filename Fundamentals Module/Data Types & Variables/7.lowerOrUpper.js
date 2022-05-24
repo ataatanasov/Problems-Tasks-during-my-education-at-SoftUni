a0797758a60c9e647f76c6f39a8ef8d201340d75
Atanas Atanasov <ataatanasov@outlook.com>
2022-05-24 23:32:20 +0300

    Added solutions to the homework tasks (Data Types & Variables)

---
+++ Fundamentals Module/Data Types & Variables/7.lowerOrUpper.js
@@ -0,0 +1,9 @@
+function lowerOrUpper(letter) {
+    
+    if (letter === letter.toUpperCase()) { // toest proverqvame ako l === L
+        console.log('upper-case');         // ideqta e che tova go pravi glavna bukva
+    } else {
+        console.log('lower-case')
+    }
+}
+lowerOrUpper('L')
