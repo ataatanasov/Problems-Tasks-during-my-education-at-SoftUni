401184c47e97eaa15cbea1f435fcd38599ee15a3
Atanas Atanasov <ataatanasov@outlook.com>
2022-06-05 21:36:45 +0300

    Added solutions to the homework tasks (Arrays)

---
+++ Fundamentals Module/Arrays/7.maxSequenceOfEqualElements.js
@@ -0,0 +1,23 @@
+function maxSequenceOfEqualElements(arr) {
+    // find the longest sequence of equal elements;
+    // if it exists, print the leftmost one;
+
+    let newArray = [] // kraen rezultat shte e tuk;
+    let firstArray = [] // 
+    for (let i = 0; i < arr.length; i++) {
+        firstArray = [] // refresh the array on every step;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                firstArray.push(arr[j]);
+            } else {
+                break;
+            }
+            if (firstArray.length > newArray.length) {
+                newArray = firstArray;
+            }
+        } 
+    }
+    console.log(newArray.join(' '));
+
+}
+maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
