6c6f8bc6b7250e254410f538c571cc4209616e41
Atanas Atanasov <ataatanasov@outlook.com>
2022-06-08 23:17:46 +0300

    Added solutions to the homework tasks (Functions)

---
+++ Fundamentals Module/Functions/1.smallestOfThreeNumbers.js
@@ -0,0 +1,13 @@
+function smallestOfThreeNumbers(num1, num2, num3) {
+
+    if (num1 < num2 && num1 < num3) {
+        console.log(num1);
+    } else if (num2 < num1 && num2 < num3) {
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4)
